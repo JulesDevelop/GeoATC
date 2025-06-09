48d3935532b5945720dcffbaaa3a9c7eacd1a464
Jules <167021550+JulesDevelop@users.noreply.github.com>
2025-06-09 23:17:01 +0200

    Add files via upload

--- loader.js
+++ loader.js
@@ -1,1 +1,13 @@
-document.write(cdn.jsdelivr.net/gh/JulesDevelop/GeoATC/index.html);
+fetch('https://cdn.jsdelivr.net/gh/JulesDevelop/GeoATC/index.html')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Erreur HTTP " + response.status);
+        }
+        return response.text();
+      })
+      .then(html => {
+        document.write(html)
+      })
+      .catch(error => {
+        console.error("Erreur lors du chargement du fichier :", error);
+      });
